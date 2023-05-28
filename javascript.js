f3311f04e53b60d5a7426ddf9a47bbffe7f6c70f
Rosario <118317818+Melly2929@users.noreply.github.com>
2023-05-28 19:36:18 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+let Nombreusuario
+
+do { 
+    Nombreusuario= prompt ("Ingrese su nombre");
+    alert("Bienvenid@ a la pagina" +" "+ Nombreusuario);
+}
+while ( Nombreusuario != null);
+
+
+ 
+
+
+
